c655953d7c53e0ad05b078026fe7a8ecf5f58e0a
Vanessa Mañas Margalef <vmama.91095@gmail.com>
2019-07-15 00:13:28 +0200

    Change the behaviour of cards.

--- js/site.js
+++ js/site.js
@@ -1,28 +1,80 @@
 
 /** Initialise all variables **/
-/*let site_index = document.getElementsByClassName('site_index')[0];
-let index_cards = document.getElementsByClassName('site_cards')[0];
-let logo = document.getElementsByClassName('site_logo')[0];
-let title = document.getElementsByClassName('site_title')[0];
+// General
+let site_index = document.getElementsByClassName('site_index')[0];
 let site_header = document.getElementsByClassName('site_header')[0];
-*/
 
+// Characters
+let index_cards = document.getElementsByClassName('site_cards')[0];
+let character_details = document.getElementsByClassName('site_character')[0];
+let selected_card = document.getElementsByClassName('site_selected_card');
+let site_card_images = document.getElementsByClassName('site_card_image');
+let site_card_row = index_cards.getElementsByClassName('row');
 
-// Click on search
+// Search
 let search = document.getElementsByClassName('site_search')[0];
 let search_submit = search.getElementsByClassName('search')[0];
 let search_input = search.getElementsByTagName('input')[0];
 
+// Pagination
+let site_pagination_name = document.getElementsByClassName('site_pagination_name')[0];
+let site_pagination_previous = document.getElementsByClassName('site_pagination_previous')[0];
+let site_pagination_next = document.getElementsByClassName('site_pagination_next')[0];
+
+// Chart
+let site_chart = document.getElementById('site_chart');
+
+// Other
+let gender = {'Male': [], 'Female': [], 'Unknown' : []};
+let selected_image = 0;
+let site_click_character = function () {
+    let id = (this.src.split('.')[1]).split('/')[4];
+
+    if (selected_card.length === 0) {
+        this.classList.add('site_selected_card');
+        getCharacter(id);
+        character_details.style.display = 'block';
+
+    } else if (selected_card.length === 1) {
+        if (selected_card[0] !== this) {
+            selected_card[0].classList.remove('site_selected_card');
+            this.classList.add('site_selected_card');
+            getCharacter(id);
+            character_details.style.display = 'block';
+        }
+    }
+
+    selected_image = id;
+};
+
+/**
+ * Search function
+ */
 search_submit.addEventListener('click', function(){
     search_input.focus();
     search_input.value = '';
+
+    // TODO: filter by characters (when submit -button- and keypress -intro-)
 });
 
+/**
+ * Initialise web
+ */
+window.onload = function() {
+    setPage(1);
 
+    site_index.addEventListener('click', function(event){
+        if (event.target.classList[0] !== 'site_card_image') {
+            character_details.style.display = 'none';
 
-
-
-
+            if (event.target.classList[0] !== 'site_card_image') {
+                if (selected_image !== 0) {
+                    index_cards.getElementsByClassName('site_card_image card_' + selected_image)[0].classList.remove('site_selected_card');
+                }
+            }
+        }
+    });
+};
 
 
 
--- js/site_cards.js
+++ js/site_cards.js
@@ -1,162 +1,26 @@
-
-let gender = {'Male': [], 'Female': [], 'Unknown' : []};
-
-let site_click_character = function () {
-    let character_details = document.getElementsByClassName('site_character')[0];
-    character_details.style.display = 'block';
-
-    let id = this.className.split('_')[1];
-    this.style.border = 'solid #afa71d 2px';
-    getCharacter(1);
-};
-
-
 /**
  * List all characters on index.
  */
-function listCharacters()
+function listCharacters(page = null)
 {
-    fetch('https://rickandmortyapi.com/api/character')
+    let url = 'https://rickandmortyapi.com/api/character';
+    if (page !== null) {
+        url += '/?page=' + page;
+    }
+
+    fetch(url)
         .then(response => response.json())
         .then(data => {
-            console.log(data.results);
+            console.log(data);
 
-          //  let card_width = (index_cards.style.width).substring(0,(index_cards.style.width).length-2) / 5;
-          //  let card_height = (index_cards.style.height).substring(0,(index_cards.style.height).length-2) / 5;
-          //  let pixels = convertToPx(card_height, 'height');
-          //  let row = 1;
-          //  let cards_row = null;
-
-            if (data.results !== null && data.results !== undefined) {
-                for (let i=0; i<data.results.length; i++) {
-            //    let linebreak = false;
-
-
-                 /*   if (i === 0) {
-                        let next_row = document.createElement('div');
-                        next_row.className = 'row site_cards_row row_'+row;
-                        index_cards.appendChild(next_row);
-                        cards_row = next_row;
-
-                    } else if (i % 5 === 0 && i !== 0) {
-                        linebreak = true;
-
-                        let next_row = document.createElement('div');
-                        row += 1;
-                        next_row.className = 'row site_cards_row row_'+row;
-                        index_cards.appendChild(next_row);
-                        cards_row = next_row;
-                    } else {
-                        cards_row = document.getElementsByClassName('site_cards_row row_'+row)[0];
-                    }*/
-
-                  /*  let card = document.createElement('div');
-                    card.className = 'card card_'+i;
-                    card.style.width = (card_width - 2) + 'vw';
-                    card.style.height = convertPxTo(pixels+10, 'height') + 'vh';
-                    card.style.textAlign = 'center';
-                    index_cards.appendChild(card);
-
-                    let image = document.createElement('img');
-                    image.src = data.results[i]['image'];
-                    image.id = 'character_'+(i+1);
-                    image.style.width = pixels +'px';
-                    image.style.height = (pixels) +'px';
-                    image.style.position = 'fixed';
-                    card.appendChild(image);*/
-
-
-                    if (data.results[i]['gender'] === 'Male') {
-                        gender['Male'].push(i);
-                    } else if (data.results[i]['gender'] === 'Female') {
-                        gender['Female'].push(i);
-                    } else {
-                        gender['Unknown'].push(i);
-                    }
-
-
-
-
-
-                /*        let cards = document.getElementsByClassName('card');
-                        console.log(cards);
-
-                        for (let j = 0; j < cards.length; j++) {
-                            cards[j].addEventListener('click', site_click_character);
-
-                            /*cards[j].addEventListener('mouseout', function () {
-                                this.style.border = '0';
-                            });*/
-                       // }
-
-                     //   cards[0].click();
-
-
-
-                }
-                fillGenderChart(data.results.length);
-
-                    getCharacter(1);
-
-
-
-
-
-
-
+            if (data.results.length  > 0) {
+                setAllCards(data);
+                setPagination(data);
             }
-
-
-
-
-
-/*
-
-            let leg_name_male = document.getElementsByClassName('male')[0];
-            let leg_name_female = document.getElementsByClassName('female')[0];
-            let leg_name_unknown = document.getElementsByClassName('unknown')[0];
-
-            leg_name_male.addEventListener('mouseover', function () {
-                let cards = document.getElementsByClassName('card');
-
-                for (let j = 0; j < gender['male'].length; j++) {
-                    cards[j].hover();
-                }
-            });
-
-            leg_name_female.addEventListener('mouseover', function () {
-                prova2.style.borderTopColor = 'purple';
-                prova1.style.borderTopColor = 'green';
-                prova3.style.borderTopColor = 'green';
-            });
-
-            leg_name_unknown.addEventListener('mouseover', function () {
-                prova3.style.borderTopColor = 'purple';
-                prova2.style.borderTopColor = 'green';
-                prova1.style.borderTopColor = 'green';
-            });
-
-            leg_name_male.addEventListener('mouseout', function () {
-                prova1.style.borderTopColor = 'green';
-                prova2.style.borderTopColor = 'green';
-                prova3.style.borderTopColor = 'green';
-            });
-
-            leg_name_female.addEventListener('mouseout', function () {
-                prova2.style.borderTopColor = 'green';
-                prova1.style.borderTopColor = 'green';
-                prova3.style.borderTopColor = 'green';
-            });
-
-            leg_name_unknown.addEventListener('mouseout', function () {
-                prova3.style.borderTopColor = 'green';
-                prova2.style.borderTopColor = 'green';
-                prova1.style.borderTopColor = 'green';
-            });
-*/
-
         })
-        .catch();
+        .catch(function(err) {
+            console.error(err);
+        });
 }
 
 /**
@@ -165,6 +29,8 @@ function listCharacters()
  */
 function fillGenderChart(num_characters)
 {
+    clearGenderChart();
+
     let chart = document.createElement('div');
     let bar_chart = document.createElement('table');
     let bar_row = document.createElement('tr');
@@ -197,7 +63,59 @@ function fillGenderChart(num_characters)
     bar_chart.appendChild(bar_row);
     bar_chart.appendChild(legend);
     chart.appendChild(bar_chart);
-    document.getElementById('site_chart').innerHTML = chart.outerHTML;
+    site_chart.innerHTML = chart.outerHTML;
+
+    hoverCardsByGender();
+}
+
+/**
+ * Clear gender chart.
+ */
+function clearGenderChart()
+{
+    site_chart.innerHTML = '';
+    console.log(site_chart);
+}
+
+/**
+ * Hover effect to the bars on bar chart. All items that belong to that gender group must be highlighted.
+ */
+function hoverCardsByGender()
+{
+    let bars = document.getElementsByClassName('bars')[0].getElementsByTagName('div');
+
+    if (bars.length > 0) {
+        for (let i = 0; i < bars.length; i++) {
+            let class_name = bars[i].className.charAt(0).toUpperCase() + bars[i].className.slice(1);
+
+            bars[i].addEventListener('mouseover', function () {
+                console.log('num: ' +selected_image);
+                if (selected_image !== 0) {
+                    console.log('selected card');
+                    console.log(selected_card[i]);
+
+                    if (selected_card.length > 0) {
+                        selected_card[0].classList.remove('site_selected_card');
+                    }
+                }
+
+                for (let i = 0; i < gender[class_name].length; i++) {
+                    site_card_images[gender[class_name][i]].classList.add('site_selected_card');
+                }
+            });
+
+            bars[i].addEventListener('mouseout', function () {
+                for (let i = 0; i < gender[class_name].length; i++) {
+                    site_card_images[gender[class_name][i]].classList.remove('site_selected_card');
+                }
+
+                if (selected_image !== 0) {
+                    console.log('selected card');
+                    document.getElementsByClassName('site_card_image card_' + selected_image)[0].classList.add('site_selected_card');
+                }
+            });
+        }
+    }
 }
 
 /**
@@ -209,29 +127,27 @@ function getCharacter(id)
     fetch('https://rickandmortyapi.com/api/character/'+id)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-
             if (data) {
                 let details = document.getElementsByClassName('site_character_details');
 
                 for (let j = 0; j < details.length; j++) {
                     if (j === 0) {
-                        let image = document.getElementsByClassName('site_character_image');
+                        let image = document.getElementsByClassName('site_character_image')[0];
                         image.src = data['image'];
 
                     } else {
                         switch (j) {
                             case 1:
-                                details[j].innerHTML += data['name'];
+                                details[j].innerHTML = details[j].innerHTML.split(':')[0] + ': ' + data['name'];
                                 break;
                             case 2:
-                                details[j].innerHTML += data['location']['name'];
+                                details[j].innerHTML = details[j].innerHTML.split(':')[0] + ': ' + data['location']['name'];
                                 break;
                             case 3:
-                                details[j].innerHTML += data['species'];
+                                details[j].innerHTML = details[j].innerHTML.split(':')[0] + ': ' + data['species'];
                                 break;
                             case 4:
-                                details[j].innerHTML += data['gender'];
+                                details[j].innerHTML = details[j].innerHTML.split(':')[0] + ': ' + data['gender'];
                                 break;
                             default:
                                 break;
@@ -240,5 +156,186 @@ function getCharacter(id)
                 }
             }
         })
-        .catch();
+        .catch(function(err) {
+            console.error(err);
+        });
+}
+
+/**
+ * Set the correct pagination params.
+ * @param page
+ */
+function setPagination(page)
+{
+  /*  site_pagination_name.innerHTML = getPaginator(data) + ' / ' + data['info']['pages'];
+
+    if (data['info']['prev'] === '') {
+        site_pagination_previous.style.display = 'none';
+    } else if (data['info']['prev']) {
+        site_pagination_previous.style.display = 'block';
+    }
+
+    if (data['info']['next'] === '') {
+        site_pagination_next.style.display = 'none';
+    } else if (data['info']['next']) {
+        site_pagination_next.style.display = 'block';
+    }*/
+
+    site_pagination_previous.addEventListener('click', function () {
+      /*  console.log('PREV: '+getPaginator(data));
+
+        let paginator = 1;
+        if (getPaginator(data) !== paginator) {
+            paginator = getPaginator(data) - 1;
+        }*/
+
+        console.log(selected_card);
+        if (selected_card.length > 0) {
+            selected_card[0].classList.remove('site_selected_card');
+
+        }
+
+        gender = {'Male': [], 'Female': [], 'Unknown' : []};
+        setPage(page-1);
+
+
+       // setPage(getPaginator(data));
+        site_pagination_previous.removeEventListener('click', function () {});
+    });
+
+    site_pagination_next.addEventListener('click', function () {
+
+        console.log(selected_card);
+        if (selected_card.length > 0) {
+            selected_card[0].classList.remove('site_selected_card');
+
+        }
+
+       // console.log('NEXT: '+getPaginator(data));
+
+      /*  let paginator = data['info']['pages'];
+        console.log('max paginator nextt' + paginator);
+        if (getPaginator(data) !== paginator) {
+            paginator = getPaginator(data) + 1;
+            console.log('max paginator next' + paginator);
+        }
+
+        setPage(paginator);
+*/
+
+        if (page === 25) {
+            setPage(25);
+        } else {
+            setPage(page+1);
+        }
+
+        site_pagination_next.removeEventListener('click', function () {});
+    });
+}
+
+function getPaginator(data)
+{
+    console.log('iddd' + data['results'][data['results'].length-1]['id']);
+    console.log('getpaginator: '+ data['results'][data['results'].length-1]['id'] / Math.round(data['info']['count']/data['info']['pages']));
+
+    console.log('resultats / pàgines '+ data['results'][data['results'].length-1]['id'] + '/' +Math.round(data['info']['count']/data['info']['pages']));
+    return data['results'][data['results'].length-1]['id'] / Math.round(data['info']['count']/data['info']['pages']);
+}
+
+/**
+ * Show the correct page according to direction (previous or next).
+ * @param page
+ */
+function setPage(page) {
+    if (page === 0) {
+        page = 1;
+    } else if (page === 26) {
+        page = 25;
+    }
+    console.log('SET PAGE: '+page);
+
+    fetch('https://rickandmortyapi.com/api/character/?page='+page)
+        .then(response => response.json())
+        .then(data => {
+            if (data) {
+                // Show or hide arrows
+                if (data['info']['prev'] === '') {
+                    site_pagination_previous.style.display = 'none';
+                } else if (data['info']['prev']) {
+                    site_pagination_previous.style.display = 'block';
+                }
+
+                if (data['info']['next'] === '') {
+                    site_pagination_next.style.display = 'none';
+                } else if (data['info']['next']) {
+                    site_pagination_next.style.display = 'block';
+                }
+
+                site_pagination_name.innerHTML = page + ' / ' + data['info']['pages'];
+
+                setAllCards(data);
+
+                site_pagination_previous.addEventListener('click', function () {
+
+                    if (selected_card.length > 0) {
+                        selected_card[0].classList.remove('site_selected_card');
+
+                    }
+
+                    setPage(page-1);
+
+                    site_pagination_previous.removeEventListener('click', function () {});
+                });
+
+                site_pagination_next.addEventListener('click', function () {
+
+                    if (selected_card.length > 0) {
+                        selected_card[0].classList.remove('site_selected_card');
+
+                    }
+
+                    setPage(page+1);
+
+                    site_pagination_next.removeEventListener('click', function () {});
+                });
+
+
+            }
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
+}
+
+/**
+ * List all cards and generate the chart according to results.
+ * @param data
+ */
+function setAllCards(data)
+{
+    gender = {'Male': [], 'Female': [], 'Unknown' : []};
+    console.log('SET ALL CARDS!!!');
+    character_details.style.display = 'none';
+
+    console.log('length images: '+site_card_images.length);
+
+    for (let i=0; i<site_card_images.length; i++) {
+        site_card_images[i].src = data.results[i].image;
+        site_card_images[i].style.display = 'block';
+        site_card_images[i].addEventListener('click', site_click_character);
+
+        if (data.results[i]['gender'] === 'Male') {
+            gender['Male'].push(i);
+        } else if (data.results[i]['gender'] === 'Female') {
+            gender['Female'].push(i);
+        } else {
+            gender['Unknown'].push(i);
+        }
+    }
+
+    console.log(gender['Male'].length);
+    console.log(gender['Female'].length);
+
+    fillGenderChart(data.results.length);
+   // setPagination(page);
 }
